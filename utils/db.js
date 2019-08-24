f81463386d83a8168d98e87a53621554b56f6482
maximomiranda <maximomirandah@gmail.com>
2019-08-24 14:04:44 -0500

    Hot fixed: update urlDB in mongoose.connect funtion

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 // Requires
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/motomax', { useNewUrlParser: true, useCreateIndex: true }, (err) => {
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
 
     if (err) throw err
 
